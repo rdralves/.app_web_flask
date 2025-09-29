70fa3d771fcdb8df092988cd8636997286808128
rdralves <rdr.alves@gmail.com>
2025-09-29 15:34:31 -0300

    Atualização do arquivo routes.py, recebendo dados do formulario do arquivo index.htm

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@main_bp.route('/', methods=['GET', 'POST'])
 def home():
     hello = "Hello, World! Hello _Flask!"
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        print(f"Name: {name}, Email: {email}, Password: {password}")
+    
+    
     return render_template('index.html', hello=hello)
