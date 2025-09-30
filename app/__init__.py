3da55c70657027c3de00594a29c9495573b6cca4
rdralves <rdr.alves@gmail.com>
2025-09-30 08:32:20 -0300

    Atualização do arquivo __init__.py do diretorio app, configuração do sqlalchemy, bem como codigo para gerar as tabelas

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 from flask import Flask
+from .models import db  # importa o db do seu models.py
 
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuration settings
+    # Configurações
     app.config['SECRET_KEY'] = 'your_secret_key'
+    # ou outro banco como PostgreSQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meubanco.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # Inicializa o banco
+    db.init_app(app)
+
+    # Registra blueprint
     from .routes import main_bp
     app.register_blueprint(main_bp)
+
+    # Cria as tabelas
+    with app.app_context():
+        db.create_all()
 
     return app
