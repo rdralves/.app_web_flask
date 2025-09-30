32a2c610c8492097d66f7e3975a51b054c31132c
rdralves <rdr.alves@gmail.com>
2025-09-30 09:18:05 -0300

    Atualizaçao no arquivo routes.py, inclusão do código que salva os dados no banco de dados no endpoint raiz

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request
-from werkzeug.security import generate_password_hash 
+from werkzeug.security import generate_password_hash
 from app.models import Usuario, db
 main_bp = Blueprint('main_bp', __name__)
+
 
 @main_bp.route('/', methods=['GET', 'POST'])
 def home():
@@ -10,10 +11,11 @@ def home():
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
-        
+
         password_hash = generate_password_hash(password)
+
+        print(f"Name: {name}, Email: {email}, Password: {password_hash}")
         
-        print(f"Name: {name}, Email: {email}, Password: {password_hash}")  
         # Cria o usuário e salva no banco
         novo_usuario = Usuario(nome=name, email=email)
         novo_usuario.set_senha(password)
