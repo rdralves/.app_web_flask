39c5ab543ec8a4124f1c800f2e190a0b9648a8b8
rdralves <rdr.alves@gmail.com>
2025-10-01 10:06:42 -0300

    Criação do template base, com estilização bootstrap5, inclusão de mensagens de feedback com o flash, tanto no servidor no arquivo app/routes.py como no lado do cliente nos templates

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,43 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash
 from app.models import Usuario, db
+
 main_bp = Blueprint('main_bp', __name__)
 
 
-@main_bp.route('/', methods=['GET', 'POST'])
-def home():
+@main_bp.route('/')
+def index():
     hello = "Hello, World! Hello _Flask!"
+    return render_template('index.html', hello=hello)
+
+
+@main_bp.route('/home', methods=['GET', 'POST'])
+def home():
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
 
-        password_hash = generate_password_hash(password)
+        if not name or not email or not password:
+            flash("Todos os campos são obrigatórios.", "warning")
+            return render_template('cadastro.html')
 
-        print(f"Name: {name}, Email: {email}")
+        password_hash = generate_password_hash(password)
 
         try:
             if Usuario.query.filter_by(email=email).first():
-                print("Erro: Email já cadastrado.")
+                flash("Erro: Email já cadastrado.", "danger")
             else:
-                # Cria o usuário e salva no banco
                 novo_usuario = Usuario(
                     nome=name, email=email, senha_hash=password_hash)
-                # novo_usuario.set_senha(password)
                 db.session.add(novo_usuario)
                 db.session.commit()
 
-                print(f"Usuário {name} salvo com sucesso!")
+                flash(f"Usuário {name} salvo com sucesso!", "success")
+                return redirect(url_for('main_bp.index'))
         except Exception as e:
             print(f"Erro ao salvar usuário: {e}")
+            flash("Erro interno ao salvar usuário.", "danger")
 
-    return render_template('index.html', hello=hello)
+    return render_template('cadastro.html')
+# update_routes
