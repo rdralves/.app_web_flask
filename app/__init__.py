5fe57ea4fd29f99de251f71b4faf98b5fe2b869a
rdralves <rdr.alves@gmail.com>
2025-10-02 20:19:59 -0300

    Importação do flask_migrate bem como criando a instância dele no app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .models import Usuario, db  # importa o db do seu models.py
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 
 def create_app():
@@ -11,12 +12,11 @@ def create_app():
     # ou outro banco como PostgreSQL
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cadastro.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     # Configuração do Flask-Login
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'main_bp.login'  # 👈 define a rota de login
-
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -24,7 +24,7 @@ def create_app():
 
     # Inicializa o banco
     db.init_app(app)
-
+    migrate = Migrate(app, db)  # Inicializa o Flask-Migrate
     # Registra blueprint
     from .routes import main_bp
     app.register_blueprint(main_bp)
