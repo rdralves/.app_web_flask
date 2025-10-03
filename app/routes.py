2f2f821d84fc5a0671244a9cf3d19d8881d065c0
rdralves <rdr.alves@gmail.com>
2025-10-02 21:31:53 -0300

    corrigindo uma importação, sistema testado e sem bugs até aqui

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from datetime import datetime
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
